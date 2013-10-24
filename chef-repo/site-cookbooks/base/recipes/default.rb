c4b2114c5aa7796bfa614b8e35ba8a4ee00cfbe7
keisatou <dummy>
2013-10-25 02:56:08 +0900

    first commit

--- chef-repo/site-cookbooks/base/recipes/default.rb
+++ chef-repo/site-cookbooks/base/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: base
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+
+service 'iptables' do
+    action [:disable, :stop]
+end
